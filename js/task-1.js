26a151bc356baa59a100ecb558addf8c8b85046a
NatyKos <145201378+NatyKos@users.noreply.github.com>
2024-02-01 19:57:01 +0000

    .

--- js/task-1.js
+++
@@ -1,8 +0,0 @@
-const items = document.querySelectorAll('.item')
-console.log(`Number of categories: ${items.length}`);
-items.forEach(item => {
-    const name = item.querySelector('h2');
-    console.log(`Category: ${name.textContent}`);
-    const number = item.querySelectorAll('li');
-    console.log(`Elements: ${number.length}`); 
-});
